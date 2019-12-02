68439daa6b71ecf7cffd8b0ab8682f252604897e
Dharam Singh <jsracer71@gmail.com>
2019-12-02 12:06:18 -0500

    first

---
+++ app/src/main/java/com/example/dropdownd/PizaaActivity.java
@@ -0,0 +1,14 @@
+package com.example.dropdownd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PizaaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizaa);
+    }
+}
